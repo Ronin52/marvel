5c70af1d9626c7b08e77385ad629a6e1dfdfcd9c
ronin52 <shuninkirill52@gmail.com>
2019-12-06 11:55:31 +0300

    Initial commit

---
+++ src/test/java/ru/ronin52/marvel/MarvelApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ronin52.marvel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
